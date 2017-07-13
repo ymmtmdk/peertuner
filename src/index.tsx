a8b89c3f61e2ee4f68fbfb7b322a8df25ac48adb
ymmtmdk <ymmtmdk@gmail.com>
2017-07-13 16:16:13 +0900

    no comment

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { PeerJs } from "./components/PeerJs";
-// import { Communicator } from "./communicator";
 import { Communicator } from "peer-communicator";
-// import { Tuner } from "./tuner";
 import { JsTunerUI, Recorder } from "jstuner-ui";
 
 const APIKEY = 'c86fbf19-fbee-4b25-80ea-02b27155ec51';
@@ -27,15 +25,15 @@ async function main(){
     const tuner = new Recorder();
     const tunerUI = new JsTunerUI(document.getElementById("main"));
     tuner.main();
-    tuner.onData = (wave, hz, note)=>{
-      tunerUI.draw(wave, hz, note);
+    tuner.onData = (wave, hz)=>{
+      tunerUI.draw(wave, hz);
     }
     await communicator.prepare();
     communicator.on("accept", e=>{
       console.log("accept");
-      tuner.onData = (wave, hz, note)=>{
+      tuner.onData = (wave, hz)=>{
         communicator.send(hz);
-        tunerUI.draw(wave, hz, note);
+        tunerUI.draw(wave, hz);
       };
     });
   }
